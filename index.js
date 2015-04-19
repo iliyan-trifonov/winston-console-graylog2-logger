bc5868dcd0fd2d92caa77b7520ddad1f62af4d56
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-04-19 13:37:36 +0300

    Added proper configuration and restructured the module

--- index.js
+++ index.js
@@ -1,48 +1,54 @@
 'use strict';
 
-var config = require('./config.json'),
+var config,
     winston = require('winston'),
     moment = require('moment'),
     log2console,
     log2graylog;
 
-if (config.logger.enable) {
-    winston.loggers.add('log2console', {
-        console: {
-            colorize: true,
-            timestamp: function () {
-                return moment().format("D MMM HH:mm:ss")
-            },
-            handleExceptions: true,
-            humanReadableUnhandledException: true
-        }
-    });
-    log2console = winston.loggers.get('log2console');
-    if (config.logger.graylog2.enable) {
-        var WinstonGraylog2 = require('winston-graylog2');
-        winston.loggers.add('log2graylog', {
-            transports: [
-                new (WinstonGraylog2)({
-                    name: 'Chat',
-                    handleExceptions: true,
-                    graylog: {
-                        //TODO: put the facility in config
-                        facility: 'NodeJS Chat App',
-                        servers: [{
-                            host: config.logger.graylog2.host,
-                            port: config.logger.graylog2.port
-                        }]
-                    }
-                })
-            ]
+function Logger (cnf) {
+    if (!cnf) {
+        throw new Error('Invalid configuration!');
+    }
+    config = cnf;
+    if (config.enable) {
+        winston.loggers.add('log2console', {
+            console: {
+                colorize: true,
+                timestamp: function () {
+                    return moment().format("D MMM HH:mm:ss")
+                },
+                handleExceptions: true,
+                humanReadableUnhandledException: true
+            }
         });
-        log2graylog = winston.loggers.get('log2graylog');
-        log('info', 'Using graylog2', config.logger.graylog2);
+        log2console = winston.loggers.get('log2console');
+        if (config.graylog2 && config.graylog2.enable) {
+            var WinstonGraylog2 = require('winston-graylog2');
+            winston.loggers.add('log2graylog', {
+                transports: [
+                    new (WinstonGraylog2)({
+                        name: 'Chat',
+                        handleExceptions: true,
+                        graylog: {
+                            //TODO: put the facility in config
+                            facility: 'NodeJS Chat App',
+                            servers: [{
+                                host: config.graylog2.host,
+                                port: config.graylog2.port
+                            }]
+                        }
+                    })
+                ]
+            });
+            log2graylog = winston.loggers.get('log2graylog');
+            log('info', 'Using graylog2', config.graylog2);
+        }
     }
 }
 
 function log(level, msg, meta) {
-    if (!config.logger.enable) {
+    if (!config.enable) {
         return false;
     }
     if (!meta) {
@@ -56,10 +62,12 @@ function log(level, msg, meta) {
     }
 }
 
-exports.info = function (msg, meta) {
+Logger.prototype.info = function (msg, meta) {
     log('info', msg, meta);
 };
 
-exports.error = function (msg, meta) {
+Logger.prototype.error = function (msg, meta) {
     log('error', msg, meta);
 };
+
+module.exports = Logger;
